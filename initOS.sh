8c9eb0022016b8275057c803a2659da23c4ee06f
ZhouXingTuan <appletreezxt@qq.com>
2017-11-11 13:17:26 +0800

    c

--- initOS.sh
+++ initOS.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
-yum install gcc-c++
+# yum install gcc-c++
 
-yum -y install openssl*
+# yum -y install openssl*
 
-yum install mysql*
+# sudo yum search mariadb
 
+# yum install mariadb mariadb-sever
+
+# yum install mariadb*
+
+# yum install mariadb-embedded mariadb-libs mariadb-bench
+
+# systemctl enable mariadb
+
+# systemctl start mariadb.service
+
+# systemctl stop mariadb.service
+
+# /data/mysql
+# 更改data目录后，执行
+# chown -R mysql:mysql /data/mysql/
+
+
+# 卸载
+# yum -y remove mariadb*
 
