101b57a2fd8c75a100b356b890c62d0ff5207a1d
zxt <zxt@96.com>
2018-01-03 17:53:34 +0800

    c

--- tool/parser.lua
+++ tool/parser.lua
@@ -93,7 +93,7 @@ function parser:parseFile(file, save_file, print_flag)
 var %s=%s;
 for(var name in %s){
 	eproto.register(name, %s[name]);
-};
+}
 ]]
 	local js_file = name..".js"
 	local js_buf = string.format(js_temp, name, json_buf, name, name)
